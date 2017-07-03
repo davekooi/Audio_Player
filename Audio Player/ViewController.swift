c7d90f723d19133f706ef9d1c704ec251859ce3d
David Kooistra <davidkooistra@davids-macbook-pro-2.local>
2017-07-03 00:50:34 -0600

    Finished Audio Player

--- Audio Player/ViewController.swift
+++ Audio Player/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
+    @IBAction func play(_ sender: Any) {
+        player.play()
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        player.pause()
+    }
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        player.volume = slider.value
+    }
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = Bundle.main.path(forResource: "sheep", ofType: "mp3")
+        
+        do{
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+        } catch {
+            
+            // Process Errors
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
